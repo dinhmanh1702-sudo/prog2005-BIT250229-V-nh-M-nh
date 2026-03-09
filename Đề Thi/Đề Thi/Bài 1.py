0df190c1a852906a0851d67363a9319253e44ee3
dinhmanh1702-sudo <dinhmanh1702@gmail.com>
2026-03-09 15:03:19 +0700

    Add files via upload

---
+++ Đề Thi/Đề Thi/Bài 1.py
@@ -0,0 +1,12 @@
+# Nhập 3 số
+a = float(input("Nhập số a: "))
+b = float(input("Nhập số b: "))
+c = float(input("Nhập số c: "))
+
+# Tìm số lớn nhất và nhỏ nhất
+max_num = max(a, b, c)
+min_num = min(a, b, c)
+
+# In kết quả
+print("Số lớn nhất là:", max_num)
+print("Số nhỏ nhất là:", min_num)
