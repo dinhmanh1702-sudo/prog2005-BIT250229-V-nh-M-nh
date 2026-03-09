4985258218c3aaadaf197c3ce94a8c8fe2afd08d
dinhmanh1702-sudo <dinhmanh1702@gmail.com>
2026-03-09 15:00:33 +0700

    Add files via upload

---
+++ Đề Thi/bai5.py
@@ -0,0 +1,32 @@
+while True:
+    print("\n===== MENU =====")
+    print("1. Bài 1")
+    print("2. Bài 2")
+    print("3. Bài 3")
+    print("4. Bài 4")
+    print("0. Thoát")
+
+    choice = input("Chọn chương trình: ")
+
+    if choice == "1":
+        print("Bạn đã chọn Bài 1")
+        # Code bài 1 đặt ở đây
+
+    elif choice == "2":
+        print("Bạn đã chọn Bài 2")
+        # Code bài 2 đặt ở đây
+
+    elif choice == "3":
+        print("Bạn đã chọn Bài 3")
+        # Code bài 3 đặt ở đây
+
+    elif choice == "4":
+        print("Bạn đã chọn Bài 4")
+        # Code bài 4 đặt ở đây
+
+    elif choice == "0":
+        print("Thoát chương trình.")
+        break
+
+    else:
+        print("Lựa chọn không hợp lệ, vui lòng chọn lại!")
