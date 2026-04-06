cdc311a3e43f85e22fc1ba3d1926a068a560a81b
dinhmanh1702-sudo <dinhmanh1702@gmail.com>
2026-04-06 15:05:19 +0700

    Add files via upload

---
+++ bt/bai1.py
@@ -0,0 +1,7 @@
+r = float(input("Nhập bán kính: "))
+
+PI = 3.14
+
+chu_vi = 2 * PI * r
+
+print("Chu vi hình tròn là:", chu_vi)
---
+++ bt/bai2.py
@@ -0,0 +1,10 @@
+a = int(input("Nhập số thứ nhất: "))
+b = int(input("Nhập số thứ hai: "))
+
+tong = a + b
+hieu = a - b
+tich = a * b
+
+print("Tổng:", tong)
+print("Hiệu:", hieu)
+print("Tích:", tich)
